86203333f0874964dad4ece5601ba1ed2af3927f
zrdominguez <zechariahd@gmail.com>
2025-02-10 14:28:44 -0600

    solved question 1 of W3D3-a from DSA practice

---
+++ W3-D3-a/my-solution.js
@@ -0,0 +1,14 @@
+const arr1=[10, 3, 5, 6, 20];
+const arr2=[-10, -3, -5, -6, -20];
+const arr3=[1, -4, 3, -6, 7, 0];
+
+function maxOfThree(arr){
+  const sortedNums = arr.sort((a, b) => b-a)
+  console.log(sortedNums)
+  let len = sortedNums.length
+  let product1 = sortedNums[0]*sortedNums[len-1]*sortedNums[len-2];
+  let product2 = sortedNums[0]*sortedNums[1]*sortedNums[2];
+  return product1 > product2 ? product1 : product2
+}
+
+console.log(maxOfThree(arr1))
