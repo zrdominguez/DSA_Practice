019c37d70bb7e2f3da744072035c2de716fc441e
zrdominguez <zechariahd@gmail.com>
2025-02-26 14:12:14 -0600

    solved makeChange formula with recursion but is not the most time efficient

--- W1-D5-a/solution.js
+++ W1-D5-a/solution.js
@@ -1,16 +1,26 @@
 const arr1 = [10, 7 , 1]
-const arr2 = [25, 10, 5, 1]
+const arr2 = [1, 2, 5]
 
-// function makeChange(amount, coins){
-//   //find if coin is a factor of amount
-//   //combination nested for loop
-//   //amount is < 0
-//   //amount is == 0
+function makeChange(amount, coins){
 
-//   if(amount == 0) return //something
-//   else if()
-//   else
-// }
+  if(amount === 0) return 0
+  if(amount < 0) return Infinity
 
-const str = "flow";
-console.log(str.slice(0,10));
+  let minCoins = Infinity;
+
+  for (let coin of coins) {
+    let result = makeChange(amount - coin, coins);
+
+    if (result !== Infinity) {
+      minCoins = Math.min(minCoins, 1 + result);
+    }
+  }
+
+  return minCoins === Infinity || minCoins === 0 ? -1 : minCoins
+
+}
+
+console.log(makeChange(14, arr1))
+console.log(makeChange(11, arr2))
+console.log(makeChange(3, [2]))
+console.log(makeChange(0, [1]))
